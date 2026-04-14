42c05408f51113db1388e2b69ce048a622973496
Margarit Petrosyan <magymag@mail.ru>
2026-04-14 15:21:57 +0400

    Changes

---
+++ database/migrations/2026_04_12_130552_add_profile_fields_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+         $table->string('status')->nullable();
+         $table->text('creative_corner')->nullable();
+         $table->text('favorite_books')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+       $table->dropColumn(['status','creative_corner','favorite_books']);
+        });
+    }
+};
